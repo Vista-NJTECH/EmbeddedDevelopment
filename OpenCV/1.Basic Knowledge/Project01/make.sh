d7d98ca67a10533fd009931f2ce7ae901cb003c7
cherryweddel <yang9624@yahoo.com>
2022-03-17 11:52:30 +0800

    add two examples

---
+++ OpenCV/1.Basic Knowledge/Project01/make.sh
@@ -0,0 +1,3 @@
+cd build/
+make
+./PROJECT01
---
+++ OpenCV/1.Basic Knowledge/Project02/build/CMakeFiles/PROJECT02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PROJECT02.dir/project02.cpp.o"
+  "PROJECT02.pdb"
+  "PROJECT02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PROJECT02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
